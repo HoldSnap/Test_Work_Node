d39f5db70f3015dcf9ff2231813b79c6bd776f0b
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-09 14:23:19 +0300

    refactor(controllers): create getAllArticles

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -9,3 +9,12 @@ exports.createArticle = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getAllArticles = async (req, res) => {
+    try {
+        const articles = await Article.findAll();
+        res.status(200).json(articles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
