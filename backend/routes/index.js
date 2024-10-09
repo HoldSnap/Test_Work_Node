55e965a34007e77d809e81224d210eeee2bc4b64
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-09 14:16:48 +0300

    feat(routes): create index_file

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const articleRoutes = require('./articleRoutes');
+const commentRoutes = require('./commentRoutes');
+
+const router = express.Router();
+
+router.use('/article', articleRoutes);
+
+router.use('/article/:articleId/comments', commentRoutes);
+
+module.exports = router;
