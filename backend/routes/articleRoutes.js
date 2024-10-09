8314e9288c9c201893b49db12380a1a7ece55284
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-09 14:19:13 +0300

    refactor(routes): update artilceRouter

--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,9 +1,16 @@
-
 const express = require('express');
 const articleController = require('../controllers/articleController');
+
 const router = express.Router();
 
 router.post('/', articleController.createArticle);
 
+router.get('/', articleController.getAllArticles);
+
+router.get('/:id', articleController.getArticleById);
+
+router.patch('/:id', articleController.updateArticle);
+
+router.delete('/:id', articleController.deleteArticle);
 
 module.exports = router;
