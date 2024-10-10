547302fc8af53066cf2694e8762da67db964068c
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-10 16:46:39 +0300

    refactor: change html stuctures in ArticleList

--- frontend/src/components/article/ArticleList.vue
+++ frontend/src/components/article/ArticleList.vue
@@ -1,50 +1,65 @@
 <template>
-    <v-container>
-      <v-list>
-        <v-list-item v-for="article in articles" :key="article.id">
-          <v-list-item-content>
-            <v-list-item-title>{{ article.title }}</v-list-item-title>
-            <v-list-item-subtitle>
-              <span>Created at: {{ formatDate(article.createdAt) }}</span>
-              <span> | </span>
-              <span>Updated at: {{ formatDate(article.updatedAt) }}</span>
-            </v-list-item-subtitle>
-            <v-list-item-text>
-              {{ article.content }}
-            </v-list-item-text>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-container>
-  </template>
-  
-  <script>
-  import { onMounted,computed } from 'vue';
-  import { useStore } from 'vuex'; 
-  
-  export default {
-    setup() {
-      const store = useStore();
-      const articles = computed(() => store.getters.allArticles); 
-  
-      const fetchArticles = async () => {
-        await store.dispatch('fetchArticles'); 
-      };
-  
-      const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-        return new Date(dateString).toLocaleDateString('en-US', options);
-      };
-  
-      onMounted(() => {
-        fetchArticles();
-      });
-  
-      return {
-        articles,
-        formatDate,
-      };
-    },
-  };
-  </script>
-  
+  <v-container>
+    <v-btn @click="goToAddArticle" color="primary">Добавить статью</v-btn>
+    <v-list>
+      <v-list-item v-for="article in articles" :key="article.id">
+        <v-list-item-content>
+          <v-list-item-title>{{ article.title }}</v-list-item-title>
+          <v-list-item-subtitle>
+            <span>Created at: {{ formatDate(article.createdAt) }}</span>
+            <span> | </span>
+            <span>Updated at: {{ formatDate(article.updatedAt) }}</span>
+          </v-list-item-subtitle>
+          <v-list-item-text>
+            {{ article.content }}
+          </v-list-item-text>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn @click="goToEditArticle(article.id)" color="secondary">Редактировать</v-btn> 
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+import { onMounted, computed } from 'vue';
+import { useStore } from 'vuex'; 
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+    const articles = computed(() => store.getters.allArticles); 
+
+    const fetchArticles = async () => {
+      await store.dispatch('fetchArticles'); 
+    };
+
+    const formatDate = (dateString) => {
+      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+      return new Date(dateString).toLocaleDateString('en-US', options);
+    };
+
+    const goToAddArticle = () => {
+      router.push('/article/new'); 
+    };
+
+    const goToEditArticle = (id) => {
+      router.push(`/article/${id}`); 
+    };
+
+    onMounted(() => {
+      fetchArticles();
+    });
+
+    return {
+      articles,
+      formatDate,
+      goToAddArticle,
+      goToEditArticle,
+    };
+  },
+};
+</script>
