34ed8b533e98014033ef4249e1102ea63694c85f
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-10 12:20:20 +0300

    fix: disable CORS

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
