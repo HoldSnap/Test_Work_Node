ad1328f96a19ae0ff26a54a7819fe98dbe78fa6e
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-10-10 12:00:15 +0300

    feat(Vuetify): base html for ArticleList

---
+++ frontend/src/components/article/ArticleList.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <v-list>
+        <v-list-item v-for="article in articles" :key="article.id">
+          <v-list-item-content>
+            <v-list-item-title>{{ article.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-container>
+  </template>
+  
+  <script>
+
+  </script>
+  
